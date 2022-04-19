db1dc617e805053b84d6092d08b23739f3c58484
albertord84 <albertord84@gmail.com>
2022-04-18 23:35:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-weehsd-azhumj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-weehsd-azhumj)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', loadChildren: () => RecipesModule },
+  {
+    path: 'shopping-list',
+    loadChildren: () => ShoppingListModule
+  },
+  {
+    path: 'auth',
+    loadChildren: () => AuthModule
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
